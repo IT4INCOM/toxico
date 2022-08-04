1a06a3bf45eb216c87ed389888840c51394fb2ff
Hsandoval15 <hsandoval@it4u.com.mx>
2022-08-04 11:26:28 -0500

    cambio

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,44 +1,48 @@
-import React, {useState, useContext} from 'react'
-import { db } from '../../firebase/firebase'
-import { collection, addDoc, serverTimestamp, doc, updateDoc } from 'firebase/firestore'
-//import {cartContext} from '../../context/cartContext'
-
+import React, { useState, useContext } from "react";
+import { db } from "../../firebase/firebase";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 
 const Cart = () => {
-
   //const {products} = useContext(cartContext)
 
-  const [idVenta, setIdVenta] = useState("")
+  const [idVenta, setIdVenta] = useState("");
 
   const datosComprador = {
-    nombre: 'Juan',
-    apellido: 'Perez',
-    email: 'juanperez@gmail.com',
-  }
+    nombre: "Juan",
+    apellido: "Perez",
+    email: "juanperez@gmail.com",
+  };
 
   const finalizarCompra = () => {
-    const ventasCollection = collection(db, 'ventas');
+    const ventasCollection = collection(db, "ventas");
     addDoc(ventasCollection, {
       datosComprador,
-      items: [{ nombre: "papa",id:1 }, { nombre: "batata",id:2 }],
+      items: [
+        { nombre: "papa", id: 1 },
+        { nombre: "batata", id: 2 },
+      ],
       date: serverTimestamp(),
-      total: 500
-    })
-    .then((result) =>{
-      setIdVenta(result.id)
-    })
-    
-    const updateCollection = doc(db, "productos","S5K3OHXUmjlcye7tzKef");
-    updateDoc(updateCollection,{stock:10})
+      total: 500,
+    }).then((result) => {
+      setIdVenta(result.id);
+    });
 
-  }
+    const updateCollection = doc(db, "productos", "S5K3OHXUmjlcye7tzKef");
+    updateDoc(updateCollection, { stock: 10 });
+  };
 
   return (
     <>
       <div>Cart</div>
       <button onClick={finalizarCompra}>Concretar Compra</button>
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
