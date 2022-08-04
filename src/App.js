48d7ad74c2f33f698b7581551a80e8f4602e0879
Hsandoval15 <hsandoval@it4u.com.mx>
2022-08-04 11:16:47 -0500

    Update App.js
    base

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from "react";
-import './App.css';
-import Navbar from "./Components/Navbar/Navbar"
-import ItemListContainer from './Containers/ItemListContainer/ItemListContainer'
-import ItemDetailsContainer from './Containers/ItemDetailsContainer/ItemDetailsContainer'
-import Cart from './Components/Cart/Cart'
-import CartCustomProvider from './Context/CartContext'
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import ItemListContainer from "./Containers/ItemListContainer/ItemListContainer";
+import ItemDetailsContainer from "./Containers/ItemDetailContainer/ItemDetailsContainer";
+import Cart from "./Components/Cart/Cart";
+import CartCustomProvider from "./Context/CartContext";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-
   return (
-      <BrowserRouter>
-        <CartCustomProvider >
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<ItemListContainer greeting='Bienvenido' />} />
-            <Route path="/category/:categoryId" element={<ItemListContainer greeting='Bienvenido' />} />
-            <Route path="/product/:productId" element={<ItemDetailsContainer />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </CartCustomProvider>
-      </BrowserRouter>
-  )
-}
+    <BrowserRouter>
+      <CartCustomProvider>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer greeting="Bienvenido" />}
+          />
+          <Route
+            path="/category/:categoryId"
+            element={<ItemListContainer greeting="Bienvenido" />}
+          />
+          <Route
+            path="/product/:productId"
+            element={<ItemDetailsContainer />}
+          />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </CartCustomProvider>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
