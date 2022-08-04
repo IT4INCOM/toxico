73f89f1637ff7ea0e94861510cfb47ef5b8c4e24
Hsandoval15 <hsandoval@it4u.com.mx>
2022-08-04 10:19:10 -0500

    r

--- src/Containers/ItemDetailContainer/ItemDetailsContainer.js
+++ src/Containers/ItemDetailContainer/ItemDetailsContainer.js
@@ -1,36 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import CircularProgress from '@mui/material/CircularProgress';
-import ItemDetail from './ItemDetail';
-import { useParams } from 'react-router-dom';
-import { db } from "../../firebase/firebase"
-import { doc, getDoc, collection } from "firebase/firestore"
+import React, { useEffect, useState } from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import ItemDetail from "./ItemDetail";
+import { useParams } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import { doc, getDoc, collection } from "firebase/firestore";
 
 export const ItemDetailsContainer = () => {
+  const [product, setProduct] = useState([]);
+  const [loaded, setLoaded] = useState(true);
 
-    const [product, setProduct] = useState([]);
-    const [loaded, setLoaded] = useState(true)
+  const { productId } = useParams();
 
-    const { productId } = useParams();
+  useEffect(() => {
+    const productsCollection = collection(db, "productos");
+    const refDoc = doc(productsCollection, productId);
+    getDoc(refDoc)
+      .then((result) => {
+        setProduct({
+          id: result.id,
+          ...result.data(),
+        });
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setLoaded(false));
+  }, [productId]);
 
-    useEffect(() => {
-        const productsCollection = collection(db, 'productos');
-        const refDoc = doc(productsCollection, productId)
-        getDoc(refDoc).then(result => {
-            setProduct({
-                id: result.id,
-                ...result.data(),
-            })
-        })
-        .catch(err => console.log(err))
-        .finally(() => setLoaded(false))
-    }, [productId]);
+  return (
+    <>
+      {loaded ? (
+        <CircularProgress color="success" />
+      ) : (
+        <ItemDetail product={product} />
+      )}
+    </>
+  );
+};
 
-    return (
-        <>
-            {loaded ? <CircularProgress color="success" /> : <ItemDetail product={product} />}
-        </>
-    )
-}
-
-
-export default ItemDetailsContainer
+export default ItemDetailsContainer;
